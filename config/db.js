a9a6c42159bb69aaa9699fd6827a831b56c81887
mrbaummath <baum1286@gmail.com>
2022-11-04 14:53:05 -0400

    dotenv

--- config/db.js
+++ config/db.js
@@ -1,5 +1,8 @@
 'use strict'
 
+require("dotenv").config()
+
+
 // creating a base name for the mongodb
 // REPLACE THE STRING WITH YOUR OWN DATABASE NAME
 const mongooseBaseName = 'express-books-deliverable'
